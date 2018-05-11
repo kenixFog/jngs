86d1e1667e97af4f1c22fa4a4f9628d59132ab82
kenxiFog <414737645@qq.com>
2018-05-11 15:54:14 +0800

    1

---
+++ src/com/whjn/sysManage/service/impl/SysFileServiceImpl.java
@@ -0,0 +1,13 @@
+package com.whjn.sysManage.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.whjn.common.service.impl.BaseServiceImpl;
+import com.whjn.sysManage.model.po.SysFile;
+import com.whjn.sysManage.service.SysFileService;
+
+@Service
+public class SysFileServiceImpl extends BaseServiceImpl<SysFile> implements SysFileService{
+
+
+}
